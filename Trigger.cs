4a484dc998aed208486ab7a1c6cc5db6fffcf414
Online-kuyk <60877921+Online-kuyk@users.noreply.github.com>
2020-02-20 12:08:55 +0800

    Create Trigger.cs

---
+++ Trigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Trigger : MonoBehaviour
+{
+	void OnTriggerEnter(Collider coll)
+	{
+		if (coll.gameObject.name == "Player")
+		{
+			SceneManager.LoadScene("gameover");
+		}
+	}
+}
