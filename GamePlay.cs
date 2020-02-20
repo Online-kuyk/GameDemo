235aac9b3e648a3ede5a890c3efc6263e599797c
Online-kuyk <60877921+Online-kuyk@users.noreply.github.com>
2020-02-20 12:05:34 +0800

    Create GamePlay.cs

---
+++ GamePlay.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GamePlay : MonoBehaviour
+{
+    public void GoNextScene(string Name)
+    {
+        SceneManager.LoadScene(Name);
+    }
+}
